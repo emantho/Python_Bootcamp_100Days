4ee6ffb8833720ae338ac19cab8dd93ed793a94d
Eder Manjarres <emanja1014@gmail.com>
2022-02-15 09:09:57 -0300

    Day 3 part 1

---
+++ day-3_lesson-29_if-else.py
@@ -0,0 +1,20 @@
+# if / else
+
+print("welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("you can ride the Roallercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+# Compoarative Operators
+'''
+>   Greater Than 
+<   Lesser than
+>=  Greater than or equal to
+<=  Lesser than or iqual to
+==  Is equal
+!=  Is different
+'''
---
+++ day-3_lesson-31_Nested-if-elif.py
@@ -0,0 +1,29 @@
+
+# if nested
+
+print('Welcome to the Rollercoaster!')
+height = int(input('What is your height in cm? '))
+
+if height >= 120:
+    print('you can ride the rollercoaster!')
+    age = int(input('What is your age? '))
+    if age <= 18:
+        print('Please pay $7.')
+    else:
+        print('Please pay $12.')
+else:
+    print('Sorry, you have to grow taller before you can ride.')
+
+# elif
+
+if height >= 120:
+    print('you can ride the rollercoaster!')
+    age = int(input('What is your age? '))
+    if age < 12:
+        print('Please pay $5.')
+    elif age <= 18:
+        print('Please pay $7.')
+    else:
+        print('Please pay $12.')
+else:
+    print('Sorry, you have to grow taller before you can ride.')
