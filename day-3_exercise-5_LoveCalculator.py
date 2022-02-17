c7526416a2f029ef016176d50956345de91eb873
Eder Manjarres <emanja1014@gmail.com>
2022-02-17 09:30:08 -0300

    changes over lovecalculator

--- day-3_exercise-5_LoveCalculator.py
+++ day-3_exercise-5_LoveCalculator.py
@@ -1,5 +1,6 @@
 from os import system
-system('clear')
+#system('clear')
+system("cls")
 
 # 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
@@ -8,39 +9,46 @@ name2 = input("What is their name? \n").lower()
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-t,r,u,e = 0, 0, 0, 0 
-l,o,v,e = 0, 0, 0, 0
-
-print(name1)
-print(name2)
+true = 0 
+love = 0
 
 if 't' in name1 or 'r' in name1 or 'u' in name1 or 'e' in name1 or 't' in name2 or 'r' in name2 or 'u' in name2 or 'e' in name2:
-    if 't' in name1 or 't' in name2:
-        t += 1
-        print(f'T={t}')
-    elif 'r' in name1 or 'r' in name2:
-        r += 1
-        print(f'R={r}')
-    elif 'u' in name1 or 'u' in name2:
-        u += 1
-        print(f'U={u}')
-    elif 'e' in name1 or 'e' in name2:
-        e += 1
-        print(f'E={e}')
+   
+    true += name1.count("t")
+    true += name2.count("t")
+  
+    true += name1.count("r")
+    true += name2.count("r")
+   
+    true += name1.count("u")
+    true += name2.count("u")
+   
+    true += name1.count("e")
+    true += name2.count("e")
+    
 
-elif 'l' in name1 or 'o' in name1 or 'v' in name1 or 'e' in name1 or 'l' in name2 or 'o' in name2 or 'v' in name2 or 'e' in name2:
-    if 'l' in name1 or 'l' in name2:
-        l += 1
-        print(f'L={l}')
-    elif 'o' in name1 or 'o' in name2:
-        o += 1
-        print(f'O={o}')
-    elif 'v' in name1 or 'v' in name2:
-        v += 1
-        print(f'V={v}')
-    elif 'e' in name1 or 'e' in name2:
-        e += 1
-        print(f'E={e}')
+if 'l' in name1 or 'o' in name1 or 'v' in name1 or 'e' in name1 or 'l' in name2 or 'o' in name2 or 'v' in name2 or 'e' in name2:
+    love += name1.count("l")
+    love += name2.count("l")
+   
+    love += name1.count("o")
+    love += name2.count("o")
+    
+    love += name1.count("v")
+    love += name2.count("v")
+    
+    love += name1.count("e")
+    love += name2.count("e")
 
-score = t+r+u+e+l+o+v+e
-print(score)
+
+loveScore = int(str(true) + str(love))
+if loveScore < 10 or loveScore > 90:
+    print(f"Your score is {loveScore}, you go together like coke and mentos.")
+
+elif 40 <= loveScore <= 50:
+    print(f"Your score is {loveScore}, you are already togeter.")
+else:
+    print(f"Your score is {loveScore}.")
+   
+
+#print(f"result is: true{true} y love{love} total{true + love}")
