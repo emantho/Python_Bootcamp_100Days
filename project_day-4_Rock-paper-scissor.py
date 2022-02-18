e823d0328290d8324569b6be5bdf174d60cf2d1d
Eder Manjarres <emanja1014@gmail.com>
2022-02-17 22:20:39 -0300

    Finished project day 4

--- project_day-4_Rock-paper-scissor.py
+++ project_day-4_Rock-paper-scissor.py
@@ -3,8 +3,7 @@ from random import randint, random
 
 system('clear')
 
-input = int(input('Type 0 for Rock, 1 for Paper or 2 for Scissors.\nWhat do you choose? '))
-
+game_sets = 0
 # Rock
 rock = """
     _______
@@ -35,32 +34,44 @@ scissor = """
 ---.__(___)
 """
 
-if input == 0:
-    print(rock)
+while game_sets < 3:
+    user_input = int(input('Type 0 for Rock, 1 for Paper or 2 for Scissors.\nWhat do you choose? '))
+    game_sets += 1
 
-elif input == 1:
-    print(paper)
+    if user_input == 0:
+        print(rock)
 
-elif input == 2:
-    print(scissor)
+    elif user_input == 1:
+        print(paper)
 
-else: 
-    print('That is not an option!!')
+    elif user_input == 2:
+        print(scissor)
 
-print('Computer Choose:')
-random_int = randint(0, 2)
+    else: 
+        print('That is not an option!!')
 
-if random_int == 0:
-    print(rock)
+    print('Computer Choose:')
+    random_int = randint(0, 2)
 
-elif random_int == 1:
-    print(paper)
+    if random_int == 0:
+        print(rock)
 
-elif random_int == 2:
-    print(scissor)
+    elif random_int == 1:
+        print(paper)
 
-# if input == 0:
-#     print('Is a tie!')
-# elif input == 
-# #print('You win')
-# #print('You Lose')
+    elif random_int == 2:
+        print(scissor)
+    
+    else: 
+        print('...')
+    
+
+    if user_input == random_int:
+        print('\tIs a tie!\n')
+    elif (user_input == 0 and random_int == 2) or (user_input == 1 and random_int == 0) or (user_input == 2 and random_int == 1):
+        print('\tYou win\n')
+    else: 
+        print('\tYou Lose\n')
+       
+
+print('\nThanks for play, goodbye')
