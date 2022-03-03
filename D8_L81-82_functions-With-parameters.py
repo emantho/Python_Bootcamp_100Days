4870934316c73a45db6d30995469921b51b505d6
Eder Manjarres <emanja1014@gmail.com>
2022-03-03 00:14:59 -0300

    Initiated D7

---
+++ D8_L81-82_functions-With-parameters.py
@@ -0,0 +1,23 @@
+from os import system; system("clear")
+
+# Function with parameter
+def greet(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+
+greet("Eder")
+
+# function with more than one input || ALSO CALLED Positional Argument
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is like in {location}")
+
+greet_with("Diana", "Aracataca")
+
+# function with more than one input || ALSO CALLED Keyword Argument
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is like in {location}")
+
+greet_with(name="Emilia", location="Santa Marta")
