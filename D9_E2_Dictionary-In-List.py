367a5deaa287225e3dfc5b2927f799368fa767ec
Eder Manjarres <emanja1014@gmail.com>
2022-03-07 21:24:15 -0300

    Initiated days 9

---
+++ D9_E2_Dictionary-In-List.py
@@ -0,0 +1,28 @@
+travel_log = [
+    {
+    "Country" : "France", 
+    "cities_visited" : ["Paris", "Lille", "Guijon"], 
+    "total_visits": 12 
+    },
+    {
+    "Country" : "Germany", 
+    "cities_visited" : ["Berlin", "Hamburg", "Stuttgart"], 
+    "total_visits": 5 
+    }
+]
+
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+new_country = {}
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country["country"] = country_visited
+    new_country["cities_visited"] = cities_visited
+    new_country["total_visits"] = times_visited
+    travel_log.append(new_country)
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
---
+++ D9_L1_Dictionaries.py
@@ -0,0 +1,29 @@
+
+dictionaries_structure = {"key": "value"}
+
+programming_dictionary = {
+ "Bug": "An error in a program that prevents the program from running as expected.",
+ "Function": "A piece of code that you can easily call over and over again."
+}
+
+# Retrieving items from dictionary.
+print(programming_dictionary["Bug"])
+
+# Adding new item to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+print(programming_dictionary)
+
+# Create an empty dictionary. | this method works also to wipe a dictionary
+empty_dictionary = {}
+print(empty_dictionary)
+
+# Edit an item in a dictionary
+programming_dictionary["Loop"] = "A moth in your computer"
+print(programming_dictionary)
+
+# Loopt through a dictionry
+for thing in programming_dictionary:
+    print(f"This form prints only {thing}")
+print(f"This form prints {programming_dictionary.keys()}")
+print(f"This form prints {programming_dictionary.values()}")
+print(f"This form prints {programming_dictionary.items()}")
