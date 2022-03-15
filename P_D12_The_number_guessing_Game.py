cf19b176585f099ddeae6cf68f0bc5d253a9fa04
Eder Manjarres <emanja1014@gmail.com>
2022-03-15 18:51:02 -0300

    Day 12 complete

--- P_D12_The_number_guessing_Game.py
+++ P_D12_The_number_guessing_Game.py
@@ -4,11 +4,6 @@ import random
 
 system("Clear")
 
-# global variables start
-#attemps = 0
-user_attemps = 0
-# global variables end
-
 def random_number():
     to_guess = random.randint(1,100)
     return to_guess
@@ -22,34 +17,43 @@ def game_dificult(user_difficulty):
         return attemps
     
 # main program start
+new_game = 'y'
+while new_game == 'y':
+    system("clear")
+    user_attemps = 0
 
-print(logo)
+    print(logo)
 
-print("I'm thinking in a number between 1 to 100.")
+    print("I'm thinking in a number between 1 to 100.")
 
-user_diff_chosed = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    while True:
+        user_diff_chosed = input("Choose a difficulty. Type 'easy' or 'hard': ")
+        if user_diff_chosed != 'easy' and user_diff_chosed != 'hard':
+            print(f"{user_diff_chosed} That's not an option")
+        else:
+            break
+            
+    attemps_message = print(f"You have {game_dificult(user_diff_chosed)} attemps remaining to guess the number.")
+    inner_game = True
+    to_guess = random_number()
 
-attemps_message = print(f"You have {game_dificult(user_diff_chosed)} attemps remaining to guess the number.")
-inner_game = True
-to_guess = random_number()
-
-while inner_game:
-    user_guess = int(input("Make a guess: "))
-    user_attemps += 1
-    if game_dificult(user_diff_chosed) == user_attemps:
-        print(f"YOU LOSE !! ")
-        inner_game = False
-    else:
-        if user_guess == to_guess:
-            print(f"YOU WIN !! ")
+    while inner_game:
+        user_guess = int(input("Make a guess: "))
+        user_attemps += 1
+        if game_dificult(user_diff_chosed) == user_attemps:
+            print(f"YOU LOSE !! ")
+            print(f"The hidden number is: {to_guess}")
             inner_game = False
-        elif user_guess < to_guess:
-            print("Too low")
-        elif user_guess > to_guess:
-            print("Too high")
-        
-new_game = input("Wanna play again? Type 'y' or 'n': ")
-if new_game == 'y':
-
-
+        else:
+            if user_guess == to_guess:
+                print(f"YOU WIN !! ")
+                inner_game = False
+            elif user_guess < to_guess:
+                print("Too low")
+            elif user_guess > to_guess:
+                print("Too high")
+            
+    new_game = input("Wanna play again? Type 'y' or 'n': ")
+    if new_game == 'n':
+        break
 # main program end
