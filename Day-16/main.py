81b418a2f40b09372234fe4d29e9abe68cf8e399
Eder Manjarres <emanja1014@gmail.com>
2022-03-24 07:35:03 -0300

    added files from udemy

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,29 +1,3 @@
-
-# importing module turtle
-import turtle
-
-# using turtle module from Turtle class
-timmy = turtle.Turtle()
-
-# Simplifying this
-
-# importing Turtle class directly from turtle module
-from turtle import Turtle, Screen
-emi = Turtle()
-print(emi)
-emi.shape("turtle")
-emi.color("blue")
-emi.forward(100)
-emi.position()
-
-# --- Using objects ---
-
-my_screen = Screen()
-
-# Object > atribute
-print(my_screen.canvheight)
-
-# object > method
-my_screen.exitonclick()
-
-
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
