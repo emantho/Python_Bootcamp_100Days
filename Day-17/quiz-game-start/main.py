c349099af3868953c8d3e6f84b8f5e060aefb830
Eder Manjarres <emanja1014@gmail.com>
2022-03-24 22:37:40 -0300

    Started Day 17 and project

---
+++ Day-17/quiz-game-start/main.py
@@ -0,0 +1,15 @@
+from os import system; system("clear")
+from question_model import Question
+from data import question_data
+
+# TODO-2 Creating list objects -> creating question bank
+question_bank = []
+
+#for i in range(len(question_data) - 1):
+for question in question_data:
+    question_bank.append(Question(question["text"], question["answer"]))
+
+print(question_bank[0].text)
+
+
+
---
+++ Day-17/quiz-game-start/question_model.py
@@ -0,0 +1,10 @@
+
+# TODO-1 creating question Class
+class Question:
+
+    def __init__(self, text, answer) -> None:
+        self.text = text
+        self.answer = answer
+
+
+
