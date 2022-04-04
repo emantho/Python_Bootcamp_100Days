890bd8d2647229215cc5675537d11a036c9426e1
Eder Manjarres <emanja1014@gmail.com>
2022-04-03 22:37:42 -0300

    completed day 17

--- Day-17/quiz-game-start/main.py
+++ Day-17/quiz-game-start/main.py
@@ -3,14 +3,18 @@ from os import system
 from quiz_brain import QuizBrain; system("clear")
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain 
 
 # TODO-2 Creating list objects -> creating question bank
 question_bank = []
 
-#for i in range(len(question_data) - 1):
 for question in question_data:
-    question_bank.append(Question(question["text"], question["answer"]))
+    question_bank.append(Question(question["question"], question["correct_answer"]))
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
 
-QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the Quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- Day-17/quiz-game-start/quiz_brain.py
+++ Day-17/quiz-game-start/quiz_brain.py
@@ -1,14 +1,41 @@
 
+from question_model import Question
+
+
 class QuizBrain:
     def __init__(self, q_list) -> None:
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
         
-    
+# TODO: PART 4 - Create a loop to keep asking until the end of list
+    def still_has_questions(self):
+        # if self.question_number < len(self.question_list):
+        #     return True
+        # else:
+        #     return False
+         
+        ## 👆This expression can be more shorter👇
+        return self.question_number < len(self.question_list)
+
 # TODO: asking the questions
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        input(f"Q.{self.question_number}: {}")
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer) # self it's not included as argument but yes as parameter👇
+        
 
-# TODO: checking if the answer was correct
+# TODO: PART 5 - checking if the answer was correct
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You get it right!")
+            
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
+
 # TODO: checking if we're the end of the quiz
