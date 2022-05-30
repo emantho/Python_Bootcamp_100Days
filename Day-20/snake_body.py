b19ec219b6cc848802e426cd69f4af49d98e35b0
Eder Manjarres <emanja1014@gmail.com>
2022-05-30 08:20:48 -0300

    complete snake body

--- Day-20/snake_body.py
+++ Day-20/snake_body.py
@@ -1,9 +1,31 @@
 from turtle import Turtle, Screen
 
+# Building up acreen for Snake
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
 screen.title("My Snake Game")
+
+# Creating Snake body
+
+x_initial = [0, -20, -40]
+full_snake = []
+
+for snake_index in range(0, 3):
+    new_snake = Turtle("square")
+    new_snake.color("white")
+    new_snake.speed(5)
+    new_snake.penup()
+    new_snake.goto(x=x_initial[snake_index], y=0)
+    full_snake.append(new_snake)
+
+
+game_is_on = True
+
+while game_is_on:
+    for body in full_snake:
+        body.forward(10)
 
 
 screen.exitonclick()
