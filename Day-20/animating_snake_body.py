4cce014c08f85f714d474089ec59f06b42d48529
Eder Manjarres <emanja1014@gmail.com>
2022-06-02 07:14:31 -0300

    added animation to snake

--- Day-20/animating_snake_body.py
+++ Day-20/animating_snake_body.py
@@ -6,7 +6,7 @@ import time
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-# screen.tracer(0)
+screen.tracer(0)
 screen.title("My Snake Game")
 
 # Creating Snake body
@@ -17,7 +17,6 @@ full_snake = []
 for snake_index in range(0, 3):
     new_snake = Turtle("square")
     new_snake.color("white")
-    new_snake.speed(0)
     new_snake.penup()
     new_snake.goto(x=x_initial[snake_index], y=0)
     full_snake.append(new_snake)
@@ -28,8 +27,15 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for body in full_snake:
-        body.forward(10)
+
+    # for seg_num in range(start=2, stop=0, step=-1):
+    for seg_num in range(len(full_snake)-1, 0, -1):
+        new_x = full_snake[seg_num - 1].xcor()
+        new_y = full_snake[seg_num - 1].ycor()
+        full_snake[seg_num].goto(new_x, new_y)
+
+    full_snake[0].forward(20)
+    full_snake[0].left(50)
 
 
 screen.exitonclick()
