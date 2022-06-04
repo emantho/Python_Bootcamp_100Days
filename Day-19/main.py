4e9bcf93db4941d64417a3f9fcc0babbc3edbeba
Eder Manjarres <emanja1014@gmail.com>
2022-06-04 08:10:57 -0300

    added functions to setheading to turtle

--- Day-19/main.py
+++ Day-19/main.py
@@ -5,12 +5,32 @@ eder = Turtle()
 screen = Screen()
 
 
-def move_forward():
+def forward():
     eder.forward(10)
 
 
+def up():
+    eder.setheading(90)
+
+
+def down():
+    eder.setheading(270)
+
+
+def left():
+    eder.setheading(180)
+
+
+def right():
+    eder.setheading(0)
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="space", fun=forward)
+screen.onkey(key="Up", fun=up)
+screen.onkey(key="Down", fun=down)
+screen.onkey(key="Left", fun=left)
+screen.onkey(key="Right", fun=right)
 screen.exitonclick()
 
 # %%
