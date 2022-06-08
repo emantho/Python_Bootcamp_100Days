5d3cb887362204c0d88384167e84245b0bb65807
Eder Manjarres <emanja1014@gmail.com>
2022-06-08 08:49:08 -0300

    Initiated day 22, pong project

---
+++ Day-22/main.py
@@ -0,0 +1,11 @@
+from turtle import Screen, Turtle, screensize
+
+# Building up table
+screen = Screen()
+screen.setup(width=1000, height=600)
+screen.bgcolor("black")
+screen.title("My Pong Game")
+# screen.tracer(0)
+
+
+screen.exitonclick()
