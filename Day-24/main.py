c269658cee21deac7ad9d17bbdd35c370d1f7b4a
Eder Manjarres <emanja1014@gmail.com>
2022-06-23 08:49:01 -0300

    initiated Day 24

---
+++ Day-24/main.py
@@ -0,0 +1,26 @@
+# %%
+file = open("my_file.txt")
+content = file.read()
+print(content)
+file.close()
+
+# %%
+with open("My_file.txt") as file:
+    content = file.read()
+    print(content)
+
+# %%
+# mode = r: read; w: write; a: append
+with open("My_file.txt", mode="w") as file:
+    file.write("Hello, My name is Eder. \nI'm 12 years old, and i like chocolate")
+
+# %%
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew Text.")
+
+# %%
+# A new file can be created from scratch using w mode
+with open("New_file.txt", mode="w") as file:
+    file.write("\nNew Text.")
+
+# %%
