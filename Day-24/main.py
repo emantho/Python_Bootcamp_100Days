3c280317c08f0ca9c90e87c2922de84d59ca76e0
eder Manjarres <emanja1014@gmail.com>
2022-06-30 07:22:38 -0300

    started main

--- Day-24/main.py
+++ Day-24/main.py
@@ -7,4 +7,21 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-        
+#%% Opening the letter template absolute path
+with open(r"/home/eder/projects/Python_Bootcamp_100Days/Day-24/Input/Letters/starting_letter.txt", mode="r") as letter:
+    content = letter.read()
+    print(content)
+
+#%% Opening the letter template relative path
+with open(r"Input/Letters/starting_letter.txt", mode="r") as letter:
+    content = letter.read()
+    print(content)
+# %% Opening names relative path
+with open(r"Input/Names/invited_names.txt", mode="r") as letter:
+    content = letter.read()
+    print(content)
+# %%
+letter = open(r"Input/Letters/starting_letter.txt", mode="r")
+content = letter.readlines()
+print(content)
+# %%
